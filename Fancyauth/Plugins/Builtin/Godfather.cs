9d1be658a21e9a0b15c908e101af2ccfaf727d8e
Blackhole16 <jaro.fietz@gmx.de>
2016-04-11 22:58:33 +0200

    fix compiler errors

--- Fancyauth/Plugins/Builtin/Godfather.cs
+++ Fancyauth/Plugins/Builtin/Godfather.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Entity;
+using System.Linq;
 using System.Threading.Tasks;
 using Fancyauth.API;
 using Fancyauth.API.ContextCallbacks;
@@ -17,10 +19,9 @@ namespace Fancyauth.Plugins.Builtin
             using (var context = await FancyContext.Connect())
             using (var transact = context.Database.BeginTransaction(IsolationLevel.Serializable))
             {
-                if (context.Users.Where(u => u.Id == user.UserId).select(u => u.Membership) != null)
+                if (context.Users.Where(u => u.Id == user.UserId).Select(u => u.Membership) != null)
                 {
                     await ContextCallbackManager.Register(user, new BecomeGodfather());
-                    await ContextCallbackManager.Register(user, new RevokeGodfather());
                 }
                 transact.Commit();
             }
@@ -61,8 +62,8 @@ namespace Fancyauth.Plugins.Builtin
 
         private class BecomeGodfather : IUserContextCallback
         {
-            public string Name { get { return "become-godfather" }; }
-            public string Text { get { return "Become Godfather" }; }
+			public string Name { get { return "become-godfather"; } }
+			public string Text { get { return "Become Godfather"; } }
 
             public async Task Run(IUser actor, IUserShim targetShim)
             {
@@ -70,23 +71,24 @@ namespace Fancyauth.Plugins.Builtin
                 using (var context = await FancyContext.Connect())
                 using (var transact = context.Database.BeginTransaction(IsolationLevel.Serializable))
                 {
-                    var godfather = context.Users.Include(u => u.Membership).Where(u => u.Id == actor.UserId);
+                    var godfather = await context.Users.Include(u => u.Membership).SingleAsync(u => u.Id == actor.UserId);
 
                     // if godfather is not a real member, this method call was super duper obsolete
                     if (godfather.Membership == null)
                         return;
 
-                    var guest = context.Users.Include(u => u.Membership)
+                    var guest = await context.Users.Include(u => u.Membership)
                         .Include(u => u.GuestInvite)
                         .Include(u => u.PersistentGuest)
-                        .Where(u => u.Id == target.UserId);
+                        .Where(u => u.Id == target.UserId)
+						.SingleAsync();
 
                     // if guest is already a real member, same applies
                     if (guest.Membership != null)
                         return;
 
-                    // if user is already a persistent guest, add new godfather
-                    if (guest.PersistentGuest != null && guest.PersistentGuest.Godfathers.Any(g => g.UserId == g.))
+                    // if user is already a persistent guest and the actor isn't already a godfather, add new godfather
+					if (guest.PersistentGuest != null && !guest.PersistentGuest.Godfathers.Any(g => g.UserId == actor.UserId))
                     {
                         guest.PersistentGuest.Godfathers.Add(godfather.Membership);
                     }
